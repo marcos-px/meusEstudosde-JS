ef606057b72874c979e64338d33659923ba69d90
marcos-px <marcxstx@gmail.com>
2022-08-02 21:49:44 -0300

    reorganização

---
+++ Alura/Arrays - conteudos/for-array.js
@@ -0,0 +1,9 @@
+/*Imprima o índice e a lista com os seguintes números:
+100,200,300,400,500,600*/
+
+const numeros = [100,200,300,400,500,600];
+
+for(let i=0; i<numeros.length; i++) { //só usar esse índice uma vez por código i 
+    console.log(i, numeros[i]);
+
+}
